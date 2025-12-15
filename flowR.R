bc9bd45c7fb1ecbaede77cb31de8026050b84ea5
mathiashole <mangino@fcien.edu.uy>
2025-12-15 17:29:35 -0300

    Update space

--- flowR.R
+++ flowR.R
@@ -4,10 +4,10 @@
 # Script to load configuration from a YAML file and set up the R environment
 
 suppressPackageStartupMessages({
-  library(yaml)
-  library(dplyr)
-  library(purrr)
-  library(ggplot2)
+    library(yaml)
+    library(dplyr)
+    library(purrr)
+    library(ggplot2)
 }) # Load necessary libraries
 
 args <- commandArgs(trailingOnly = TRUE)
