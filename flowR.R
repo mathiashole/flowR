e478458207367324f9433c2662d55f5dba44c065
mathiashole <mangino@fcien.edu.uy>
2025-12-22 17:53:09 -0300

    Update arrow

--- flowR.R
+++ flowR.R
@@ -313,7 +313,7 @@ for (g in genomes) {
         geom_point(size = 3, alpha = 0.8) +
         annotate("text", x = Inf, y = Inf, label = paste0("n = ", n_anchors), hjust = 1.1, vjust = 1.5, size = 4) +
         scale_x_continuous(breaks = -window:window, labels = c(paste0("5′ -", window:1), gene_query, paste0("3′ +", 1:window))) +
-        labs(x = paste0("5′ ← ", gene_query, " → 3′"), y = "Número de genes", color = "Proteína") +
+        labs(x = paste0("5′ <- ", gene_query, " -> 3′"), y = "Número de genes", color = "Proteína") +
         theme_classic() +
         theme(legend.position = "bottom", axis.title = element_text(size = 12), axis.text = element_text(size = 10))
 
